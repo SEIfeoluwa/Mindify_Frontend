b1f35804dac896736838542408b9480d683138b7
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-15 12:00:12 -0400

    Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route />
+        <Route path='/login' element={ <Login /> }/>
+        <Route path='/profile' element={ <Profile /> }/>
+        <Route path='/register' element={ <Register /> }/>
       </Routes>
     </div>
   );
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import Sidebar from '../components/Sidebar'
+
+const Login = () => {
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Login
