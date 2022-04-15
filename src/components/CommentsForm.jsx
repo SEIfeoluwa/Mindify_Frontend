ed0c64084058c95798206b7f4e510ba6fc3da42f
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-04-15 16:18:40 -0400

    created getNewComment function

--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -1,2 +1,34 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
+const CommentsForm = () => {
+
+const [newComment, setNewComment ] = useState({
+    comment: ''
+})
+ 
+let { id } = useParams()
+
+  const getNewComment = async () => {
+      await axios({
+          url: `http://localhost:3001/posts/${id}`,
+          method: 'post',
+          data: newComment
+      })
+  }
+
+  const handleChange = (e) => {
+      setNewComment({...newComment, [e.target.name]: e.target.value })
+      console.log(e.target.name)
+      console.log(e.target.value)
+  }
+
+  return (
+    <div>CommentsForm</div>
+  )
+}
+
+export default CommentsForm
 
