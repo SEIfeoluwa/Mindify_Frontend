aafaa532344ecaddccbd4d20f164c9d7d4d7ce11
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-15 16:23:26 -0400

    merge

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,12 @@
+import Sidebar from "../components/Sidebar"
 
 
 const Profile = () => {
 
     return (
         <div>
-            
+            <Sidebar />
+            <div></div>
         </div>
     )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,34 @@
-
+import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+    let navigate = useNavigate()
+
+    const [formValues, setFormValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await RegisterUser({
+          name: formValues.name,
+          email:formValues.email,
+          password:formValues.password
+        })
+        setFormValues({
+          name: '',
+          email: '',
+          password: '',
+          confirmPassword: ''})
+        navigate('/login')
+    }
+
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
 
     return (
         <div>
