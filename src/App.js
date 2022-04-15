fa6c7c5c19e741049dd92230cdb64382dfcc573e
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-15 16:00:37 -0400

    save

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
+import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [teacher, setTeacher] = useState(null)
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    toggleAuthenticated(false);
+    localStorage.clear();
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div>
-      <Routes>
-        <Route path='/login' element={ <Login /> }/>
+      <Header 
+        authenticated={authenticated}
+        user={user}
+        handleLogOut={handleLogOut}
+      />
+      <main>
+       <Routes>
+        <Route path='/login' element={
+          <Login
+          setTeacher={setTeacher}
+          toggleAuthenticated={toggleAuthenticated} 
+          /> }/>
         <Route path='/profile' element={ <Profile /> }/>
         <Route path='/register' element={ <Register /> }/>
-      </Routes>
+      </Routes> 
+      </main>
     </div>
   );
 }
