9c09ca084dfe9fb23b199afac5035173bba864c1
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-04-15 14:58:08 -0400

    added axios call to UpdatePost

--- src/pages/Createpost.js
+++ src/pages/Createpost.js
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
+
+  let navigate = useNavigate()
+
+  const [ newPost, setNewPost ] = useState({
+    title: '',
+    content: '',
+    image: '',
+  })
+
+ const getNewPost = async () => {
+     await axios({
+       url: `http://localhost:3001/posts`,
+       method: 'post',
+       data: newPost
+     })
+ }
+
+ 
+
   return (
     <div></div>
   )
