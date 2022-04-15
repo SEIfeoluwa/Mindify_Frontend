163e5e4e439d3b4a0cf869f79dea10c4f871c24d
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-04-15 13:33:43 -0400

    created basic setup

---
+++ src/pages/PostDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CommentsForm from '../components/CommentsForm'
+
+const PostDetails = () => {
+  return (
+    <div>PostDetails</div>
+  )
+}
+
+export default PostDetails
