f3a9f895a63be05ac514d76e53a8e0e073d1c7d0
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-16 15:03:29 -0400

    authent

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { RegisterUser } from '../services/Auth'
 import { useNavigate } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
 
 const Register = () => {
     let navigate = useNavigate()
@@ -32,7 +33,62 @@ const Register = () => {
 
     return (
         <div>
-
+            <Sidebar />
+            <div className="card-overlay centered">
+                <form className="col" onSubmit={handleSubmit}>
+                    <div className="input-wrapper">
+                        <label htmlFor="name">Name</label>
+                        <input
+                        onChange={handleChange}
+                        name="name"
+                        type="text"
+                        placeholder="Your Name"
+                        value={formValues.name}
+                        required
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="email">Email</label>
+                        <input
+                        onChange={handleChange}
+                        name="email"
+                        type="email"
+                        placeholder="example@example.com"
+                        value={formValues.email}
+                        required
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="password">Password</label>
+                        <input
+                        onChange={handleChange}
+                        type="password"
+                        name="password"
+                        value={formValues.password}
+                        required
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input
+                        onChange={handleChange}
+                        type="password"
+                        name="confirmPassword"
+                        value={formValues.confirmPassword}
+                        required
+                        />
+                    </div>
+                    <button
+                        disabled={
+                        !formValues.email ||
+                        (!formValues.password &&
+                            formValues.confirmPassword === formValues.password)
+                        }
+                    >
+                        Sign In
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
