a6d307a31c1bcf601673539e4db941763e274f7e
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-04-19 14:58:20 -0400

    fixed create post route

--- src/pages/Createpost.js
+++ src/pages/Createpost.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import SideBar from '../components/Sidebar'
 
 const CreatePost = () => {
@@ -14,9 +14,11 @@ const CreatePost = () => {
     image: '',
   })
 
+  let { id } = useParams()
  const getNewPost = async () => {
+  console.log(newPost)
      await axios({
-       url: `http://localhost:3001/posts`,
+       url: `http://localhost:3001/posts/${id}`,
        method: 'post',
        data: newPost
      })
@@ -24,7 +26,9 @@ const CreatePost = () => {
 
  const handleChange = (e) => {
       setNewPost({...newPost, [e.target.name]: e.target.value })
-      console.log(newPost)
+     
+      console.log(e.target.name)
+      console.log(e.target.value)
  } 
 
  const handleSubmit = () => {
@@ -38,11 +42,8 @@ const CreatePost = () => {
     <div className="s-container">
        <h2 className="new-post">Add A New Post</h2>
     <div>
-
-      <SideBar />
-       
-
-          <form className="submit-form" onSubmit={handleSubmit}> 
+       <SideBar />
+        <form className="submit-form" onSubmit={handleSubmit}> 
                   <input className="input" type="text" value={newPost.title} onChange={handleChange} name={'title'} placeholder={'title'} />
                   <input className="in-cont input" type="text" value={newPost.content} onChange={handleChange} name={'content'} placeholder={'content'} />
                   <input className="input" type="text" value={newPost.image} onChange={ handleChange} name={'image'} placeholder={'image url'} />
