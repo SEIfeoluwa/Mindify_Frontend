0953e50e68dd4c8368a4adc7e88b1f36171a7ed4
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-21 11:32:31 -0400

    save sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,10 +7,11 @@ const Sidebar = ({ authenticated, teacher, handleLogOut }) => {
       authenticatedOptions = (
             <ul className="nav-links">
              <li className="item doc"><Link to="/"><i className="fa-solid fa-house"></i><span>Home</span></Link></li>
-             <li className="item doc"><i className="fa-solid fa-user"></i><Link to="/prof"><span>My Profile</span></Link></li>
-             <li className="item doc"><i className="fa-solid fa-ellipsis"></i><Link to="/IPP"><span>More</span></Link></li>
-             <li className="item"><button className="butt"><Link to="/createpost"><span>Make A Post</span></Link></button></li>
+             <li className="item doc"><Link to="/prof"><i className="fa-solid fa-user"></i><span>My Profile</span></Link></li>
+             <li className="item doc"><Link to="/planner"><i class="fa-solid fa-calendar-check"></i><span>Your Goals</span></Link></li>
+             <li className="item doc"><Link to="/createpost"><i className="fa-solid fa-square-plus"></i><span>Make A Post</span></Link></li>
              <li className="item bot"><Link onClick={handleLogOut} to="/"><i className="fa-solid fa-arrow-right-from-bracket"></i><span>Log out</span></Link></li>
+             <li className="item doc"><Link to="/IPP"><i className="fa-solid fa-ellipsis"></i><span>More</span></Link></li>
             </ul>   
       )
     }
@@ -19,6 +20,7 @@ const Sidebar = ({ authenticated, teacher, handleLogOut }) => {
           <li className="item doc"><Link to="/"><i className="fa-solid fa-house"></i><span>Home</span></Link></li>
           <li className="item doc"><Link to="/register"><i className="fa-solid fa-address-card"></i><span>Register</span></Link></li>
           <li className="item doc"><Link to="/login"><i className="fa-solid fa-arrow-right-to-bracket"></i><span>Login</span></Link></li>
+          <li className="item doc"><Link to="/planner"><i class="fa-solid fa-calendar-check"></i><span>Your Goals</span></Link></li>
           <li className="item doc"><Link to="/IPP"><i className="fa-solid fa-ellipsis"></i><span>More</span></Link></li>
           <li></li>
         </ul>
