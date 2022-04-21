4a793267e96fd402e0e75b5c56dcd426d3a5981b
Ran Kim <rannio@gmail.com>
2022-04-21 10:49:21 -0400

    merge conflicts

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -7,12 +7,8 @@ export const SignInUser = async (data) => {
     // Set the current signed in users token to localStorage
     
     localStorage.setItem('token', res.data.token)
-<<<<<<< HEAD
-    console.log(res.data.user)
-=======
     
     
->>>>>>> main
     return res.data.user
   } catch (error) {
     throw error
