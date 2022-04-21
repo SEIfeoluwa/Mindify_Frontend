c7afc5d4327d4f35c59cd0c108314f71d9336ca5
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-21 15:40:30 -0400

    save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-// import Header from './components/Header'
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
@@ -64,15 +63,8 @@ const App = () => {
 
         <Route path="/" element={ <Landing /> } />
         <Route path="/home" element={ <Home />} />
-<<<<<<< HEAD
-        <Route path="/home/posts/postdetail/:id" element={ <PostDetails /> } /> 
-=======
         <Route path="/posts/postdetail/:id" element={ <PostDetails /> } /> 
-
-        <Route path="/" element={ <Home />} />
         <Route path="/postdetail/:id" element={ <PostDetails /> } /> 
-
->>>>>>> 672368919061117290e2ddb0be939da3767a416b
         <Route path="/IPP" element={ <IPP /> } />
         <Route path="/createpost" element={ 
         <CreatePost 
