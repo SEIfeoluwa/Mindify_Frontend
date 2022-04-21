20cee4d4bd6662e33d8fce0725cf57d73f214812
chazcurlee <chaz.curlee@gmail.com>
2022-04-21 15:53:15 -0400

    register w/ used info logic added

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { RegisterUser } from '../services/Auth'
 import { useNavigate } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
+import axios from 'axios'
 
 const Register = () => {
     let navigate = useNavigate()
@@ -14,37 +15,93 @@ const Register = () => {
         password: '',
         confirmPassword: ''
     })
+    const [usernameList, setUsernameList] = useState()
+    const [emailList, setEmailList] = useState()
+    const [usedInfo, setUsedInfo] = useState(true)
+
+    useEffect(()=> {
+        const getUserInfo = async () => {
+            let usernameArr = []
+            let emailArr = []
+
+
+            let result = await axios.get(`http://localhost:3001/teacher_info`)
+            result.data.tUsernames.map((name)=>{
+                usernameArr.push(name.username)
+            })
+            result.data.tEmails.map((email)=>{
+                emailArr.push(email.email)
+            })
+            let alphaUsernameArr = usernameArr.sort((a, b) => a.localeCompare(b))
+            let alphaEmailArr = emailArr.sort((a, b) => a.localeCompare(b))
+            setUsernameList(alphaUsernameArr)
+            setEmailList(alphaEmailArr)
+
+        }
+        getUserInfo()
+
+    }, [])
+
+    const redAlert = () => {
+        setUsedInfo(!usedInfo)
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        await RegisterUser({
-          firstName: formValues.firstName,
-          lastName: formValues.lastName,
-          username: formValues.username,
-          email:formValues.email,
-          password:formValues.password
-        })
+        
+        let formName = !usernameList.includes(formValues.username)
+        let formEmail = !emailList.includes(formValues.email)
+
+            
+        
+        if(formName && formEmail){
+            await RegisterUser({
+                firstName: formValues.firstName,
+                lastName: formValues.lastName,
+                username: formValues.username,
+                email:formValues.email,
+                password:formValues.password
+              })
+              setFormValues({
+                  firstName: '',
+                  lastName: '',
+                  username: '',
+                  email: '',
+                  password: '',
+                  confirmPassword: ''})
+              navigate('/login')
+        }
         setFormValues({
-            firstName: '',
-            lastName: '',
+            firstName: formValues.firstName,
+            lastName: formValues.lastName,
             username: '',
             email: '',
             password: '',
-            confirmPassword: ''})
-        navigate('/login')
+            confirmPassword: ''
+        })
+        console.log('Used info')
+        setTimeout(redAlert, 3000)
+
+        
     }
 
     const handleChange = (e) => {
         setFormValues({ ...formValues, [e.target.name]: e.target.value })
     }
+    console.log(formValues)
+    console.log(emailList)
+    console.log(usernameList)
 
     return (
         <div>
             <Sidebar />
             <div className="card-overlay centered">
+            <h1 className="reg-title">MINDIFY</h1>
                 <form className="col" onSubmit={handleSubmit}>
-                    <div className="input-wrapper">
+                    <div className="input-wrapper1">
                         <label htmlFor="name">First Name</label>
-                        <input
+                        <br></br>
+                        <input className="input1"
                         onChange={handleChange}
                         name="firstName"
                         type="text"
@@ -53,9 +110,10 @@ const Register = () => {
                         required
                         />
                     </div>
-                    <div className="input-wrapper">
+                    <div className="input-wrapper1">
                         <label htmlFor="name">Last Name</label>
-                        <input
+                        <br></br>
+                        <input className="input1"
                         onChange={handleChange}
                         name="lastName"
                         type="text"
@@ -64,9 +122,10 @@ const Register = () => {
                         required
                         />
                     </div>
-                    <div className="input-wrapper">
+                    <div className="input-wrapper1">
                         <label htmlFor="username">Username</label>
-                        <input
+                        <br></br>
+                        <input className={usedInfo?"input1":"input1 red-alert"}
                         onChange={handleChange}
                         name="username"
                         type="text"
@@ -75,9 +134,10 @@ const Register = () => {
                         required
                         />
                     </div>
-                    <div className="input-wrapper">
+                    <div className="input-wrapper1">
                         <label htmlFor="email">Email</label>
-                        <input
+                        <br></br>
+                        <input className={usedInfo?"input1":"input1 red-alert"}
                         onChange={handleChange}
                         name="email"
                         type="email"
@@ -86,34 +146,38 @@ const Register = () => {
                         required
                         />
                     </div>
-                    <div className="input-wrapper">
+                    <div className="input-wrapper1">
                         <label htmlFor="password">Password</label>
-                        <input
+                        <br></br>
+                        <input className="input1"
                         onChange={handleChange}
                         type="password"
                         name="password"
+                        placeholder="Password"
                         value={formValues.password}
                         required
                         />
                     </div>
-                    <div className="input-wrapper">
+                    <div className="input-wrapper1">
                         <label htmlFor="confirmPassword">Confirm Password</label>
-                        <input
+                        <br></br>
+                        <input className="input1"
                         onChange={handleChange}
                         type="password"
                         name="confirmPassword"
+                        placeholder="Confirm password"
                         value={formValues.confirmPassword}
                         required
                         />
                     </div>
-                    <button
+                    <button className="s-btn1"
                         disabled={
                         !formValues.email ||
                         (!formValues.password &&
                             formValues.confirmPassword === formValues.password)
                         }
                     >
-                        Sign In
+                        Register
                     </button>
                 </form>
             </div>
