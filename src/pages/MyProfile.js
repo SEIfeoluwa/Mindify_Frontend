c91104408e087373f7ec422c99f7269cd0feac1f
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-21 16:38:49 -0400

    post Item div

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -38,7 +38,7 @@ const MyProfile = (props) => {
     const showPost = (posts) => {  //after clicking on post it will go to PostDetails
         navigate(`/postdetail/${posts.id}`)
     } 
-// onClick={() => showPost(post)}
+
     return (posts && teacherInfo) ? (
         <div>
             <div>
@@ -47,12 +47,14 @@ const MyProfile = (props) => {
             <div >
                 {posts.map((post) => (
                     <div className="home-container" 
-                    key={post.id} >
+                    key={post.id}>
+                    <div onClick={() => showPost(post)}>
                     <PostItem 
                         content={post.content}
                         image={post.image}
                         {...post}
                     />
+                    </div>
                     <div>
                         <Delete 
                         postId={post.id}
